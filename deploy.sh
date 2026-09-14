49443cb797d61b572efbe7764088452f4a956a35
nobody <nobody@unkown.com>
2026-09-14 23:49:36 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 SERVER=$1
-if [ "${SERVER}" == "" ];then
-
+if [ "${SERVER}" == "" ]; then
+  exit
 else
   CONFIG_FILE="./wrangler_${SERVER}.toml"
-  if [ -f "${CONFIG_FILE}"]; then
+  if [ -f "${CONFIG_FILE}" ]; then
     npx wrangler deploy -c "${CONFIG_FILE}"
   fi
 fi
