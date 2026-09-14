1d26e61c095f3b0448e74155de468c893195d0f2
nobody <nobody@unkown.com>
2026-09-15 00:09:18 +0800

    init

---
+++ dev.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SERVER=$1
+if [ "${SERVER}" == "" ]; then
+  exit
+else
+  CONFIG_FILE="./wrangler_${SERVER}.toml"
+  if [ -f "${CONFIG_FILE}" ]; then
+    npx wrangler dev -c "${CONFIG_FILE}"
+  fi
+fi
+
